58f0ba8d3a72fbdc68dbc242cea8e5de3bd88a85
pzinsta <voodooshake@gmail.com>
2018-02-17 20:17:55 +0200

    Add spring configuration and spring dispatcher servlet initializer

---
+++ src/main/java/com/pzinsta/springsecurity/demo/config/DemoAppConfig.java
@@ -0,0 +1,24 @@
+package com.pzinsta.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.pzinsta.springsecurity.demo")
+public class DemoAppConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        
+        internalResourceViewResolver.setPrefix("/WEB-INF/views");
+        internalResourceViewResolver.setSuffix(".jsp");
+        
+        return internalResourceViewResolver;
+    }
+}
